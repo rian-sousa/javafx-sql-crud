cd6efedfc5f2035f189f2589ebcf6b8fe9f18db9
Rian Sousa <99686158+rian-sousa@users.noreply.github.com>
2022-06-28 11:10:53 -0300

    add remove-department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,6 +1,8 @@
 package gui.util;
 
+import java.util.Optional;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
 
 public class Alerts {
@@ -11,6 +13,14 @@ public class Alerts {
         alert.setHeaderText(header);
         alert.setContentText(content);
         alert.show();
+    }
+
+    public static Optional<ButtonType> showConfirmation(String title, String content){
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        return alert.showAndWait();
     }
     
 }
